ca61f86672e0c8884f009dd4f4228146c746f4f4
Florian Leon <leon-florian@hotmail.fr>
2021-12-03 11:15:36 +0100

    fin partie 3 MS

--- StudentsListService/src/main/java/fr/insa/florianleon/MS/StudentsListService/resources/StudentListResource.java
+++ StudentsListService/src/main/java/fr/insa/florianleon/MS/StudentsListService/resources/StudentListResource.java
@@ -3,6 +3,7 @@ package fr.insa.florianleon.MS.StudentsListService.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import fr.insa.florianleon.MS.StudentsListService.model.*;
 @RequestMapping("/students")
 public class StudentListResource {
 
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("ids/{idSpeciality}")
 	public StudentIDList getIDStudent(@PathVariable("idSpeciality") String speciality) {
@@ -39,14 +42,12 @@ public class StudentListResource {
 		students.saddStudentToList(2);
 		students.saddStudentToList(3);
 		
-		//Instanciate RestTemplate for Rest calls
-		RestTemplate restTemplate = new RestTemplate();
 		int i = 0;
 		List<Student> listStudents = new ArrayList<Student>();
 		
 		while (i < students.getStudentList().size()) {
-			StudentsInfos etudInfos = restTemplate.getForObject("http://localhost:8081/student/"+i, StudentsInfos.class);
-			Evaluation eval = restTemplate.getForObject("http://localhost:8082/evaluation/"+i, Evaluation.class);
+			StudentsInfos etudInfos = restTemplate.getForObject("http://studentInfoService/student/"+i, StudentsInfos.class);
+			Evaluation eval = restTemplate.getForObject("http://studentEvalService/evaluation/"+i, Evaluation.class);
 			listStudents.add(new Student(i, etudInfos.getFirstName(), etudInfos.getLastName(), eval.getAverage()));
 			i++;
 		}
